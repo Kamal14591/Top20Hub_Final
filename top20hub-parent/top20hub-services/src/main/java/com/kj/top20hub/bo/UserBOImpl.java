8c13c8b1efe2d62cce9d3b8762b2aa399eb2aabb
Kamal Kishor Nayak <kamal.kknayak@gmail.com>
2017-08-12 21:21:10 +0530

    Contact us and User Details.. page created.. Fetching data as well

--- top20hub-parent/top20hub-services/src/main/java/com/kj/top20hub/bo/UserBOImpl.java
+++ top20hub-parent/top20hub-services/src/main/java/com/kj/top20hub/bo/UserBOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kj.top20hub.dao.UserDAO;
 import com.kj.top20hub.dto.User;
@@ -15,6 +16,7 @@ public class UserBOImpl implements UserBO {
 	private UserDAO userDao;
 	
 	@Override
+	@Transactional
 	public List<User> getAllUsers() {
 
 		List<User> user = userDao.getAlluser();
--- top20hub-parent/top20hub-web/src/main/java/com/kj/top20hub/web/controller/UserController.java
+++ top20hub-parent/top20hub-web/src/main/java/com/kj/top20hub/web/controller/UserController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.kj.top20hub.bo.ContactUsBO;
 import com.kj.top20hub.bo.UserBO;
+import com.kj.top20hub.dao.ContactUsDAO;
+import com.kj.top20hub.dto.ContactUs;
 import com.kj.top20hub.dto.User;
 
 
@@ -17,7 +23,11 @@ public class UserController {
 	
 	@Autowired
 	private UserBO userService;
-
+	
+	@Autowired
+	private ContactUsDAO contactUsService;
+	
+	
 	@RequestMapping("/list")
 	public String listUsers(Model theModel)
 	{
@@ -29,6 +39,42 @@ public class UserController {
 		return "users_details";
 	}
 	
+	@RequestMapping("/contactUs")
+	public String contactUs(Model theModel)
+	{
+		
+		ContactUs contactUs = new ContactUs();
+		theModel.addAttribute("contactUs", contactUs);
+		
+		return "contact_page";
+	}
+	
+	@PostMapping("/saveContactUs")
+	public String SaveContactUsMsg(@ModelAttribute("contactUs") ContactUs contactUs)
+	{
+		
+		contactUsService.create(contactUs);
+		
+		return "redirect:/users/contactus/messages";
+	}
+	
+	@RequestMapping("/contactUsMessages")
+	public String contactUsMessages()
+	{
+		
+		return "contactUsMessages";
+	}
+	
+	@RequestMapping("/contactus/messages")
+	public String contactUsMsg(Model theModel)
+	{
+		
+		List<ContactUs> contactUs = contactUsService.getAllMessages();
+		theModel.addAttribute("Messages", contactUs);
+		return "messages";
+		
+	}
+
 	
 	
 }
